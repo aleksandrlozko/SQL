56b4a84cf96b214e29061f104241e91ed592b69d
aleksandrlozko <44771725+aleksandrlozko@users.noreply.github.com>
2020-04-17 20:00:16 +0300

    Create population.sql

---
+++ population.sql
@@ -0,0 +1,26 @@
+-------------------------
+-- Genre table
+-------------------------
+INSERT INTO genres(genres)
+VALUES('Comedies');
+
+INSERT INTO genres(genres)
+VALUES('Kids TV');
+
+INSERT INTO genres(genres)
+VALUES('Stand-Up Comedy');
+
+INSERT INTO genres(genres)
+VALUES('Adventure');
+
+INSERT INTO genres(genres)
+VALUES('Family Movies');
+
+INSERT INTO genres(genres)
+VALUES('Documentaries');
+
+INSERT INTO genres(genres)
+VALUES('Horror');
+
+INSERT INTO genres(genres)
+VALUES('Dramas');
