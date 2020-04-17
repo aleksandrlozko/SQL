d6b88db9cd0ea165de1b88e6965ac029f78e543d
aleksandrlozko <44771725+aleksandrlozko@users.noreply.github.com>
2020-04-18 01:57:11 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -6,18 +6,14 @@ HAVING rating IN ('R', 'TV-14', 'TV-MA')
 ORDER BY amount;
 
 --TASK 2--
-SELECT movies.name, movie_genres.movie_genres AS genre, movies.duration
-FROM movies 
+SELECT movie_genres.movie_genres AS genres, round(COUNT(name) * 100 / (SELECT SUM(COUNT(movie_genres)) FROM movie_genres GROUP BY movie_genres), 1) AS persentage
+FROM movies
 JOIN movie_genres 
 ON movies.name = movie_genres.movie_name
-ORDER BY movies.duration DESC;
+GROUP BY movie_genres.movie_genres;
 
 --TASK 3--
-SELECT movies.release_year AS year, COUNT(movies.name) AS amount, movie_country.movie_country AS country
+SELECT movies.release_year AS year, COUNT(movies.name) AS amount
 FROM movies
-JOIN movie_country
-ON movies.director = movie_country.movie_director
-JOIN movie_genres 
-ON movies.name = movie_genres.movie_name
-GROUP BY movies.release_year, movie_country.movie_country
+GROUP BY movies.release_year
 ORDER BY movies.release_year;
